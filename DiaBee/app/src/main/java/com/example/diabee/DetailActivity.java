578c90aa4822fa65067bf4a9ea7f8b450ff45a6f
bukajis <petrilajakub123@gmail.com>
2024-06-09 15:44:31 +0200

    Asi ma dzigne nejde stale oblubene #6
    už nemožem fakt už nemam paru

--- DiaBee/app/src/main/java/com/example/diabee/DetailActivity.java
+++ DiaBee/app/src/main/java/com/example/diabee/DetailActivity.java
@@ -1,10 +1,24 @@
 package com.example.diabee;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.button.MaterialButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,20 +27,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+public class DetailActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView StuffGridView;
+    public DecimalFormat df = new DecimalFormat("#.##");
 
-public class DetailActivity extends AppCompatActivity {
+    private EditText weight;
+    private double result;
 
-    private ListView stuffGridView;
+    private EditText unit;
+    private String value;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        stuffGridView = findViewById(R.id.listView);
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        StuffGridView = (ListView) findViewById(R.id.listView);
+        StuffGridView.setOnItemClickListener(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         String category = getIntent().getStringExtra("category");
@@ -34,20 +57,19 @@ public class DetailActivity extends AppCompatActivity {
         JSONArray jsonArray = loadJSONFromAsset(category + ".json");
         if (jsonArray != null) {
             try {
-                ArrayList<Product> productList = new ArrayList<>();
+                ArrayList<String> dataList = new ArrayList<>();
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONArray item = jsonArray.getJSONArray(i);
                     String name = item.getString(0);
-                    String value = item.getString(2);
-                    productList.add(new Product(name, value, false));
+                    value = item.getString(2);
+                    String dataString = name + ": " + value;
+                    dataList.add(dataString);
                 }
 
-                ProductAdapter adapter = new ProductAdapter(this, productList);
-                stuffGridView.setAdapter(adapter);
-                stuffGridView.setOnItemClickListener((parent, view, position, id) -> {
-                    Product product = productList.get(position);
-                    showProductDialog(product);
-                });
+                ProductAdapter adapter = new ProductAdapter(this, dataList);
+
+                ListView listView = findViewById(R.id.listView);
+                listView.setAdapter(adapter);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -77,13 +99,167 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    private void showProductDialog(Product product) {
-        // Implementácia dialógu pre zobrazenie a úpravu produktu
-        // Pridajte kód pre zobrazenie dialógu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterCategories(newText);
+                return true;
+            }
+        });
+
+        return true;
     }
 
-    public void setSupportActionBar(Toolbar toolbar) {
-        super.setSupportActionBar(toolbar);
+    private void filterCategories(String textfind) {
+        JSONArray jsonArray = loadJSONFromAsset("vsetko.json");
+        if (jsonArray != null) {
+            try {
+                ArrayList<String> dataList = new ArrayList<>();
+                ListView listResView = findViewById(R.id.resultView);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONArray item = jsonArray.getJSONArray(i);
+                    String name = item.getString(0);
+                    if (name.toLowerCase().contains(textfind.toLowerCase()) && !textfind.isEmpty() && textfind.length() >= 3) {
+                        String value = item.getString(2);
+                        String dataString = name + ": " + value;
+                        dataList.add(dataString);
+                        listResView.setVisibility(View.VISIBLE);
+                    }
+
+                    if (!dataList.isEmpty()) {
+                        StuffGridView.setVisibility(View.GONE);
+                    } else {
+                        StuffGridView.setVisibility(View.VISIBLE);
+                    }
+                }
+                System.out.println(dataList);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+
+                listResView.setAdapter(adapter);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.category) {
+            Toast.makeText(this, "kategorie", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.oblubene) {
+            Toast.makeText(this, "oblubene", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.voda) {
+            Toast.makeText(this, "voda", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    public void kliknutie_na_item(AdapterView<?> parent, View view, int position, long id) {
+        String item = (String) parent.getItemAtPosition(position);
+        final View customLayout = getLayoutInflater().inflate(R.layout.dialog_box, null);
+
+        final TextView TitleTextView = customLayout.findViewById(R.id.Nazov);
+        final TextView SjTextView = customLayout.findViewById(R.id.sj_num);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String[] itemArray = item.split(" ");
+        String name = null;
+        if (itemArray[1].contains("(")) {
+            name = itemArray[0];
+        } else if (itemArray[2].contains("(")) {
+            name = itemArray[0] + " " + itemArray[1];
+        } else if (itemArray[3].contains("(")) {
+            name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2];
+        } else if (itemArray[4].contains("(")) {
+            name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3];
+        } else if (itemArray[5].contains("(")) {
+            name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3] + " " + itemArray[4];
+        } else {
+            name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3] + " " + itemArray[4] + " " + itemArray[5];
+        }
+
+        value = itemArray[itemArray.length - 1];
+        SjTextView.setText(value);
+        TitleTextView.setText(name);
+        builder.setView(customLayout);
+
+        final AlertDialog alertDialog = builder.create();
+
+        MaterialButton closeButton = customLayout.findViewById(R.id.close_button);
+        MaterialButton calc_Button = customLayout.findViewById(R.id.calc_Button);
+        MaterialButton delete_Button = customLayout.findViewById(R.id.delete_Button);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss(); // Zavrieť dialog
+            }
+        });
+        calc_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weight = customLayout.findViewById(R.id.editHmotnost);
+                unit = customLayout.findViewById(R.id.editSachJed);
+
+                if (weight != null) {
+                    try {
+                        value = value.replace(",", ".");
+                        value = value.replace(",", ".");
+
+                        // Parse value and weight, then calculate the result
+                        double valueParsed = Double.parseDouble(value);
+                        double weightParsed = Double.parseDouble(weight.getText().toString());
+                        double result = (valueParsed * weightParsed) / 100;
+
+                        // Format the result to #.##
+                        DecimalFormat df = new DecimalFormat("#.##");
+
+                        result = Double.parseDouble(value) * Double.parseDouble(weight.getText().toString()) / 100;
+                        result = Double.parseDouble(df.format(result));
+                        // Set the result to the unit TextView
+                        unit.setText(String.valueOf(result));
+                    } catch (NumberFormatException e) {
+                        String errorMessage = e.getMessage();
+                        System.out.println(errorMessage);
+                        Toast.makeText(getApplicationContext(), "Hmotnosť musí byť číslo", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Musíte zadať hmotnosť", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        delete_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weight = customLayout.findViewById(R.id.editHmotnost);
+                unit = customLayout.findViewById(R.id.editSachJed);
+                weight.setText("");
+                unit.setText("");
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
--- DiaBee/app/src/main/java/com/example/diabee/ProductAdapter.java
+++ DiaBee/app/src/main/java/com/example/diabee/ProductAdapter.java
@@ -1,55 +1,246 @@
 package com.example.diabee;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 
+import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class ProductAdapter extends ArrayAdapter<Product> {
-
+public class ProductAdapter extends ArrayAdapter<String> {
+    private boolean rob=false;
     private Context context;
-    private ArrayList<Product> productList;
+    private ArrayList<String> dataList;
+    String dataString = "";
+    private EditText weight;
+    private double result;
+    String name;
+    private EditText unit;
+    private String value;
 
-    public ProductAdapter(@NonNull Context context, ArrayList<Product> productList) {
-        super(context, 0, productList);
+    public ProductAdapter(@NonNull Context context, ArrayList<String> dataList) {
+        super(context, 0, dataList);
         this.context = context;
-        this.productList = productList;
+        this.dataList = dataList;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
         }
 
-        Product product = getItem(position);
+        String data = getItem(position);
 
         TextView itemName = convertView.findViewById(R.id.item_name);
         TextView itemValue = convertView.findViewById(R.id.item_value);
         CheckBox itemFavorite = convertView.findViewById(R.id.item_favorite);
 
-        itemName.setText(product.getName());
-        itemValue.setText(product.getValue());
-        itemFavorite.setChecked(product.isFavorite());
+        // Assuming the format is "name: value"
+        String[] parts = data.split(": ");
+        name = parts[0];
+        value = parts[1];
 
-        itemFavorite.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            product.setFavorite(isChecked);
-            updateFavoriteStatus(product);
+        if(overenie(name,itemFavorite)){
+            itemFavorite.setChecked(true);
+        }
+
+        itemName.setText(name);
+        itemValue.setText(value);
+
+        // Set OnClickListener for the entire item
+        convertView.setOnClickListener(view -> {
+            String item = dataList.get(position);
+            final View customLayout = LayoutInflater.from(context).inflate(R.layout.dialog_box, null);
+
+            final TextView TitleTextView = customLayout.findViewById(R.id.Nazov);
+            final TextView SjTextView = customLayout.findViewById(R.id.sj_num);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            String[] itemArray = item.split(" ");
+            name = null;
+            if (itemArray[1].contains("(")) {
+                name = itemArray[0];
+            } else if (itemArray[2].contains("(")) {
+                name = itemArray[0] + " " + itemArray[1];
+            } else if (itemArray[3].contains("(")) {
+                name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2];
+            } else if (itemArray[4].contains("(")) {
+                name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3];
+            } else if (itemArray[5].contains("(")) {
+                name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3] + " " + itemArray[4];
+            } else {
+                name = itemArray[0] + " " + itemArray[1] + " " + itemArray[2] + " " + itemArray[3] + " " + itemArray[4] + " " + itemArray[5];
+            }
+            value = itemArray[itemArray.length - 1];
+            SjTextView.setText(value);
+            TitleTextView.setText(name);
+            builder.setView(customLayout);
+
+            final AlertDialog alertDialog = builder.create();
+
+            MaterialButton closeButton = customLayout.findViewById(R.id.close_button);
+            MaterialButton calc_Button = customLayout.findViewById(R.id.calc_Button);
+            MaterialButton delete_Button = customLayout.findViewById(R.id.delete_Button);
+
+            closeButton.setOnClickListener(v -> alertDialog.dismiss());
+            calc_Button.setOnClickListener(v -> {
+                weight = customLayout.findViewById(R.id.editHmotnost);
+                unit = customLayout.findViewById(R.id.editSachJed);
+
+                if (weight.getText().toString().isEmpty()) {
+                    Toast.makeText(context, "Musíte zadať hmotnosť", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        value = value.replace(",", ".");
+
+                        double valueParsed = Double.parseDouble(value);
+                        double weightParsed = Double.parseDouble(weight.getText().toString());
+                        double result = valueParsed * weightParsed / 100;
+
+                        // Format the result to #.##
+                        DecimalFormat df = new DecimalFormat("#.##");
+
+                        result = Double.parseDouble(value) * Double.parseDouble(weight.getText().toString()) / 100;
+                        // Set the result to the unit TextView
+                        unit.setText(String.valueOf(result));
+                    } catch (NumberFormatException e) {
+                        String errorMessage = e.getMessage();
+                        System.out.println(errorMessage);
+                        Toast.makeText(context, "Hmotnosť musí byť číslo", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            delete_Button.setOnClickListener(v -> {
+                weight = customLayout.findViewById(R.id.editHmotnost);
+                unit = customLayout.findViewById(R.id.editSachJed);
+                weight.setText("");
+                unit.setText("");
+            });
+
+            alertDialog.show();
         });
 
+        // Set OnCheckedChangeListener for the CheckBox
+        itemFavorite.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (rob){
+            if (isChecked) {
+                try {
+                    String favorites = loadFavoritesFromJson();
+                    String item = dataList.get(position);
+
+                    System.out.println(item);
+
+                    String[] itemiki = item.split(":");
+
+                    favorites += "[" + itemiki[0] + ",\n" + itemiki[1] + "]";
+                    saveFavoritesToJson(favorites);
+                    System.out.println(favorites);
+                    Toast.makeText(context, "Item added to favorites", Toast.LENGTH_SHORT).show();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "Failed to add item to favorites", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                try {
+                    String favorites = loadFavoritesFromJson();
+                    System.out.println(favorites);
+                    String item = dataList.get(position);
+                    itemFavorite.setChecked(false);
+                    String[] itemiki = item.split(":");
+                    if(favorites.contains("[" + itemiki[0] + "," + itemiki[1] + "]")){
+                    favorites=favorites.replace("[" + itemiki[0] + "," + itemiki[1] + "]","");}
+                    System.out.println(favorites);
+                    saveFavoritesToJson(favorites);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }}
+        });
+        rob=true;
         return convertView;
+
     }
 
-    private void updateFavoriteStatus(Product product) {
-        // Implementujte logiku pre aktualizáciu stavu obľúbeného produktu v JSON súbore
+    private String loadFavoritesFromJson() throws IOException, JSONException {
+        String jsonString = "";
+        try {
+            // Otvorenie súboru 'data.json' na čítanie
+            FileInputStream fis = context.openFileInput("data.json");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+
+            // Načítanie dát zo súboru
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            // Zatvorenie FileInputStream
+            fis.close();
+
+            // Reťazec s načítanými dátami
+            jsonString = stringBuilder.toString();
+            dataString=jsonString;
+            System.out.println(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
+
+    private void saveFavoritesToJson(String favorites) throws IOException {
+        try {
+            // Vytvorenie (alebo otvorenie existujúceho) súboru 'data.json' v internom úložisku
+            FileOutputStream fos = context.openFileOutput("data.json", MODE_PRIVATE);
+
+            // Zapisovanie dát do súboru
+            fos.write(favorites.getBytes());
+
+            // Zatvorenie FileOutputStream
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private boolean overenie(String name,CheckBox itemFavorite){
+        try {
+            String ujo=loadFavoritesFromJson();
+            if(ujo.contains(name)){
+                return true;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
 }
